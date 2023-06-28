19d3fb5b14c7d216ea47583d8f3b04a1f5d62823
huynhchiz <huynhchi90524@gmail.com>
2023-06-28 22:38:47 +0700

    upload

--- validator.js
+++ validator.js
@@ -1,7 +1,31 @@
 function Validator(formSelector) {
     // tạo 1 object chứa tất cả các rules của form
-    let formRules = {
+    let formRules = {}
 
+    // Quy ước tạo rules
+    // -nếu có lỗi thì return 'error message'
+    // -nếu không có lỗi thì return 'undefined'
+    let validatorRules = {
+        isRequired: function (value) {
+            return vales ? undefined : 'Vui lòng nhập vào ô này'
+        },
+        isEmail: function (value) {
+            let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            return regex.test(value) ? undefined : 'Nhập đúng định dạng email'
+        },
+        min: function (min) {
+            return function (value) {
+                return value.length >= min ? undefined : `Độ dài tối thiểu phải là ${min} ký tự`
+            }
+        },
+        max: function (max) {
+            return function (value) {
+                return value.length <= max ? undefined : `Độ dài tối đa là ${max} ký tự`
+            }
+        },
+        isConfirmed: function (value) {
+            return vales ? undefined : 'Vui lòng nhập vào ô này'
+        },
     }
     
     // Lấy ra form element trong DOM từ `formSelector`
@@ -14,7 +38,8 @@ function Validator(formSelector) {
         let inputs = formElement.querySelectorAll('[name][rules]')
 
         for (let input of inputs) {
-            console.log(input.getAttribute('rules'))
+            formRules[input.name] = input.getAttribute('rules')
         }
     }
+    console.log(formRules)
 }
